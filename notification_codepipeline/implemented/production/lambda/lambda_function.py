3088dd4494bf5c339e64bb492da490b8c52fef1f
Lucas Rejanio <52427398+LucasRejanio@users.noreply.github.com>
2021-11-04 18:25:22 -0300

    fix(lambda): refactory lambda

--- notification_codepipeline/implemented/production/lambda/lambda_function.py
+++ notification_codepipeline/implemented/production/lambda/lambda_function.py
@@ -5,15 +5,18 @@ import boto3
 
 
 def lambda_handler(event, context):
-    message_str = event['Records'][0]['Sns']['Message']
-    message = json.loads(message_str)
+    ## Get message in the event
+    message = json.loads(event['Records'][0]['Sns']['Message'])
 
+    # Get infos of message
     pipeline = message['detail']['pipeline']
     state = message['detail']['state']
     stage = message['detail']['stage']
 
+    ## Set actions to null
     actions = []
 
+    ## Validadtion of state deploy
     if state == "STARTED" and stage == "Source":
         state_pipeline = "Start Deploy of "
         title = state_pipeline + pipeline
@@ -25,10 +28,10 @@ def lambda_handler(event, context):
     elif state == "FAILED":
         state_pipeline = "There was an error in the Deploy of "
         documentation = "See the documentation for troubleshooting"
-        note = "NOTE: To view the logs you need to be logged in the QA role"
-        title = state_pipeline + pipeline + "\n" + documentation + "\n" + note
+        title = state_pipeline + pipeline + "\n" + documentation
         color = "E01E5A"
         
+        ## Creating buttons to slack request
         actions = [
             {
                 "type": "button",
@@ -45,10 +48,12 @@ def lambda_handler(event, context):
     else:
         return 0
     
+    ## Formating timezone
     tz_america_sp = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
     time_now = tz_america_sp.strftime("%d-%m-%Y: %H:%M:%S")
 
-    payload_dict = {
+    ## Payload to request
+    payload = str({
         "attachments": [
             {
                 "title": title,
@@ -57,19 +62,17 @@ def lambda_handler(event, context):
                 "actions": actions
             }
         ]
-    }
+    })
 
-    payload = str(payload_dict)
-
+    ## Get paramet with slack webhooks
     client_ssm = boto3.client('ssm')
-
     parameters = client_ssm.get_parameter(
         Name='/notification/production/lambda/slack_urls',
         WithDecryption=True
     )
+    
+    ## Get value of parameter
+    url = json.loads(parameters['Parameter']['Value'])["production"]
 
-    urls_str = parameters['Parameter']['Value']
-    urls = json.loads(urls_str)
-    url = urls["production"]
-
+    ## Post message in the slack
     requests.request("POST", url, data=payload)
