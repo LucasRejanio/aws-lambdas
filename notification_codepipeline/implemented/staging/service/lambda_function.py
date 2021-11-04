845da9ea8405ee152eddbc90dc11c0a6b1a55dab
Lucas Rejanio <52427398+LucasRejanio@users.noreply.github.com>
2021-11-04 18:28:40 -0300

    fix(lambda): refactory code

--- notification_codepipeline/implemented/staging/service/lambda_function.py
+++ notification_codepipeline/implemented/staging/service/lambda_function.py
@@ -5,23 +5,22 @@ import boto3
 
 
 def lambda_handler(event, context):
-    message_str = event['Records'][0]['Sns']['Message']
-    message = json.loads(message_str)
+    ## Get message in the event
+    message = json.loads(event['Records'][0]['Sns']['Message'])
 
+     # Get infos of message
     pipeline = message['detail']['pipeline']
     state = message['detail']['state']
     stage = message['detail']['stage']
 
-    actions = []
-
+     # Get service name
     parameter = pipeline.split("-")
     service = parameter[1]
-    
-    if pipeline.__contains__("0"):
-        environment = parameter[2] + "-" + parameter[3]
-    else:
-        environment = parameter[2]
 
+    ## Set actions to null
+    actions = []
+
+    ## Validadtion of state deploy
     if state == "STARTED" and stage == "Source":
         state_pipeline = "Start Deploy of "
         title = state_pipeline + service
@@ -34,8 +33,10 @@ def lambda_handler(event, context):
         state_pipeline = "There was an error in the Deploy of "
         documentation = "See the documentation for troubleshooting"
         note = "NOTE: To view the logs you need to be logged in the QA role"
+        title = state_pipeline + service + "\n" + documentation + "\n" + note
         color = "E01E5A"
 
+        ## Creating buttons to slack request
         actions = [
             {
                 "type": "button",
@@ -50,23 +51,15 @@ def lambda_handler(event, context):
             }
         ]
 
-        if stage == "Deploy" and service == "website":
-            state_pipeline = "There was an error of aplication in the Deploy of "
-            action = {
-                "type": "button",
-                "text": "Datadog",
-                "url": f"https://app.datadoghq.com/logs?query=task_family%3A{environment}-{service}-deploy&index="
-            },
-            actions = actions.append(action)
-
-        title = state_pipeline + service + "\n" + documentation + "\n" + note
     else:
         return 0
     
+    ## Formating timezone
     tz_america_sp = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
     time_now = tz_america_sp.strftime("%d-%m-%Y: %H:%M:%S")
     
-    payload_dict = {
+    ## Payload to request
+    payload = str({
         "attachments": [
             {
                 "title": title,
@@ -75,20 +68,19 @@ def lambda_handler(event, context):
                 "actions": actions
             }
         ]
-    }
+    })
 
-    payload = str(payload_dict)
-
+    ## Get paramet with slack webhooks
     client_ssm = boto3.client('ssm')
-
     parameters = client_ssm.get_parameter(
         Name='/notification/staging/service/slack_urls',
         WithDecryption=True
     )
 
-    urls_str = parameters['Parameter']['Value']
-    urls = json.loads(urls_str)
+    ## Get value of parameter
+    urls = json.loads(parameters['Parameter']['Value'])
 
+    ## Post message in the slack
     if pipeline.__contains__("0") or pipeline.__contains__("next"):
         for key in urls.keys():
             if pipeline.__contains__(key):
